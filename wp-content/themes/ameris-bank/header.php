a30b8f8b3d8050897f840339b0716f03d09589c3
Alexis Findiesen <alexis.findiesen@agencychief.com>
2015-10-08 14:57:54 -0400

    Added some basic colors and a typekit script to the theme

--- wp-content/themes/ameris-bank/header.php
+++ wp-content/themes/ameris-bank/header.php
@@ -15,6 +15,9 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<script src="https://use.typekit.net/bjg1fuq.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
 <?php wp_head(); ?>
 </head>
 
