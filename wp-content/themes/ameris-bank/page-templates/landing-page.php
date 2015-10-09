6d07511e283e2129d49a35195aa2db95faae97cb
Alexis Findiesen <alexis.findiesen@agencychief.com>
2015-10-09 17:14:42 -0400

    New branch

--- wp-content/themes/ameris-bank/page-templates/landing-page.php
+++ wp-content/themes/ameris-bank/page-templates/landing-page.php
@@ -16,39 +16,31 @@ get_sidebar( 'left' ); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<div class="image-banner">
-					<?php the_post_thumbnail( 'full' ); ?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+					
+				<div class="select-container">
+					<?php wp_dropdown_pages( array(
+						'child_of'         => get_the_ID(),
+						'name'             => 'select_subpage',
+						'show_option_none' => 'Looking for something else?'
+					) ); ?>
+					<!-- </select> -->
 				</div>
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class( 'landing-page-content' ); ?>>
-
-					<h3><?php the_field( 'heading' ); ?></h3>
-					
-					<div class="select-container">
-						<?php wp_dropdown_pages( array(
-							'child_of'         => get_the_ID(),
-							'name'             => 'select_subpage',
-							'show_option_none' => 'Looking for something else?'
-						) ); ?>
-						<!-- </select> -->
+				<?php if ( get_field( 'featured_solutions' ) ) { ?>
+					<div class="featured-solutions">
+						<?php foreach( get_field( 'featured_solutions' ) as $post ) {
+							setup_postdata( $post ); ?>
+							<?php the_post_thumbnail( 'landing-solution' ); ?>
+							<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+							<p>
+								<?php the_field( 'short_description' ); ?>
+								<a href="<?php the_permalink(); ?>">Learn More</a>
+							</p>
+						<?php }
+						wp_reset_postdata(); ?>
 					</div>
-
-					<?php if ( get_field( 'featured_solutions' ) ) { ?>
-						<div class="featured-solutions">
-							<?php foreach( get_field( 'featured_solutions' ) as $post ) {
-								setup_postdata( $post ); ?>
-								<?php the_post_thumbnail( 'landing-solution' ); ?>
-								<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-								<p>
-									<?php the_field( 'short_description' ); ?>
-									<a href="<?php the_permalink(); ?>">Learn More</a>
-								</p>
-							<?php }
-							wp_reset_postdata(); ?>
-						</div>
-					<?php } ?>
-
-				</div><!-- #post-## -->
+				<?php } ?>
 
 			<?php endwhile; // End of the loop. ?>
 
