26b11ff90bbc28496284c7da98595ca840fca9f1
Michael Dance <michael.dance@agencychief.com>
2015-10-26 17:36:28 -0400

    Don't show resources & guides title if none exist

--- wp-content/themes/ameris-bank/template-parts/resources-guides.php
+++ wp-content/themes/ameris-bank/template-parts/resources-guides.php
@@ -1,17 +1,17 @@
+<?php if ( get_field( 'resources_guides' ) ) { ?>
 <div class="related-resources">
 	<h3 class="related-resources__heading">Resources &amp; Guides</h3>
-	<?php if ( get_field( 'resources_guides' ) ) { ?>
-		<ul class="related-resources__rotator resources-rotator">
-			<?php foreach( get_field( 'resources_guides' ) as $post ) {
-				setup_postdata( $post ); ?>
-				<li class="related-resources__resource">
-					<a href="<?php the_permalink(); ?>" class="related-resources__link">
-						<div class="related-resources__icon"><?php the_post_thumbnail( 'circle-icon' ); ?></div>
-						<div class="related-resources__title"><?php the_title(); ?></div>
-					</a>
-				</li>
-			<?php }
-			wp_reset_postdata(); ?>
-		</ul>
-	<?php } ?>
+	<ul class="related-resources__rotator resources-rotator">
+		<?php foreach( get_field( 'resources_guides' ) as $post ) {
+			setup_postdata( $post ); ?>
+			<li class="related-resources__resource">
+				<a href="<?php the_permalink(); ?>" class="related-resources__link">
+					<div class="related-resources__icon"><?php the_post_thumbnail( 'circle-icon' ); ?></div>
+					<div class="related-resources__title"><?php the_title(); ?></div>
+				</a>
+			</li>
+		<?php }
+		wp_reset_postdata(); ?>
+	</ul>
 </div><!-- .related-resources -->
+<?php } ?>
