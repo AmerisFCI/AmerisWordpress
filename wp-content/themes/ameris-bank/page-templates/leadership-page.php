c07bf7375dea5fe27ab00f4d0723a5922f853592
Alexis Findiesen <alexis.findiesen@agencychief.com>
2015-11-16 14:16:12 -0500

    Updated leadership grid classes

--- wp-content/themes/ameris-bank/page-templates/leadership-page.php
+++ wp-content/themes/ameris-bank/page-templates/leadership-page.php
@@ -57,8 +57,8 @@ get_template_part( 'template-parts/page', 'wide-banner' ); ?>
 						<div id="post-<?php the_ID(); ?>-panel" class="leadership-grid__panel panel" <?php post_class(); ?>>
 							<a href="<?php the_permalink(); ?>">
 								<?php the_post_thumbnail( 'leader' ); ?>
-								<div>
-									<h4 class="leadership-grid__name"><?php the_title(); ?></h4>
+								<div class="leadership-grid__name-group">
+									<h3 class="leadership-grid__name"><?php the_title(); ?></h3>
 									<div class="leadership-grid__position"><?php the_field( 'position' ); ?></div>
 									<div class="leadership-grid__company"><?php the_field( 'company' ); ?></div>
 								</div>
