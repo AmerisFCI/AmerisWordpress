df5c076d69f05108303dd39652b00bf5d4e25e11
Michael Dance <michael.dance@agencychief.com>
2015-11-17 14:03:09 -0500

    Added fix for Manual Image Crop plugin

--- wp-content/plugins/manual-image-crop-forked/manual-image-crop-forked.php
+++ wp-content/plugins/manual-image-crop-forked/manual-image-crop-forked.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Manual Image Crop
+Plugin Name: Manual Image Crop (Forked)
 Plugin URI: https://github.com/tomaszsita/wp-manual-image-crop
-Description: Plugin allows you to manually crop all the image sizes registered in your WordPress theme (in particular featured image). Simply click on the "Crop" link next to any image in your media library and select the area of the image you want to crop.
-Version: 1.12
+Description: Original plugin plus a fix for the Retina image generation feature.
+Version: 1.12-1.0
 Author: Tomasz Sita
 Author URI: https://github.com/tomaszsita
 License: GPL2
