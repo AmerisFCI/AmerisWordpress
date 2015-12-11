8f42ee39d7b022e3714d32f1d7a163bf75f2be27
Michael Dance <michael.dance@agencychief.com>
2015-12-11 15:45:12 -0500

    fixed overflow of leadership grid on ipad portrait size

--- wp-content/themes/ameris-bank/page-templates/leadership-page.php
+++ wp-content/themes/ameris-bank/page-templates/leadership-page.php
@@ -25,63 +25,67 @@ get_template_part( 'template-parts/page', 'wide-banner' ); ?>
 	<div class="content-below">
 		<div class="leadership-grid">
 			<h2 class="leadership-grid__heading">Executive Management</h2>
-			<ul class="og-grid leadership-grid__management">
-				<?php $management = get_field( 'executive_management' );
-				if ( $management ) {
-					foreach( $management as $post ) {
-						setup_postdata( $post );
-						?>
-						<li id="post-<?php the_ID(); ?>-panel" <?php post_class( 'leadership-grid__panel panel' ); ?>>
-							<a href="<?php the_permalink(); ?>"
-								data-largesrc="<?php $largesrc = wp_get_attachment_image_src( get_post_thumbnail_id(), 'leader' ); echo esc_attr( $largesrc[0] ); ?>"
-								data-title="<?php echo esc_attr( get_the_title() ); ?>"
-								data-position="<?php echo esc_attr( get_field( 'position' ) ); ?>"
-								data-description="<?php echo esc_attr( get_field( 'bio' ) ); ?>">
-								<div class="leadership-grid__panel-container">
-									<?php the_post_thumbnail( 'leader' ); ?>
-									<div class="leadership-grid__name-group">
-										<h3 class="leadership-grid__name"><?php the_title(); ?></h3>
-										<div class="leadership-grid__position"><?php the_field( 'position' ); ?></div>
+			<div class="og-grid-container">
+				<ul class="og-grid leadership-grid__management">
+					<?php $management = get_field( 'executive_management' );
+					if ( $management ) {
+						foreach( $management as $post ) {
+							setup_postdata( $post );
+							?>
+							<li id="post-<?php the_ID(); ?>-panel" <?php post_class( 'leadership-grid__panel panel' ); ?>>
+								<a href="<?php the_permalink(); ?>"
+									data-largesrc="<?php $largesrc = wp_get_attachment_image_src( get_post_thumbnail_id(), 'leader' ); echo esc_attr( $largesrc[0] ); ?>"
+									data-title="<?php echo esc_attr( get_the_title() ); ?>"
+									data-position="<?php echo esc_attr( get_field( 'position' ) ); ?>"
+									data-description="<?php echo esc_attr( get_field( 'bio' ) ); ?>">
+									<div class="leadership-grid__panel-container">
+										<?php the_post_thumbnail( 'leader' ); ?>
+										<div class="leadership-grid__name-group">
+											<h3 class="leadership-grid__name"><?php the_title(); ?></h3>
+											<div class="leadership-grid__position"><?php the_field( 'position' ); ?></div>
+										</div>
 									</div>
-								</div>
-							</a>
-						</li><!-- #post-## -->
-						<?php
-					}
-					wp_reset_postdata();
-				} ?>
-			</ul>
+								</a>
+							</li><!-- #post-## -->
+							<?php
+						}
+						wp_reset_postdata();
+					} ?>
+				</ul>
+			</div>
 
 			<h2 class="leadership-grid__heading">Board of Directors</h2>
 
-			<ul class="og-grid leadership-grid__board">
-				<?php $directors = get_field( 'board_of_directors' );
-				if ( $directors ) {
-					foreach( $directors as $post ) {
-						setup_postdata( $post );
-						?>
-						<li id="post-<?php the_ID(); ?>-panel" <?php post_class( 'leadership-grid__panel panel' ); ?>>
-							<a href="<?php the_permalink(); ?>"
-								data-largesrc="<?php $largesrc = wp_get_attachment_image_src( get_post_thumbnail_id(), 'leader' ); echo esc_attr( $largesrc[0] ); ?>"
-								data-title="<?php echo esc_attr( get_the_title() ); ?>"
-								data-position="<?php echo esc_attr( get_field( 'position' ) ); ?>"
-								data-company="<?php echo esc_attr( get_field( 'company' ) ); ?>"
-								data-description="<?php echo esc_attr( get_field( 'bio' ) ); ?>">
-								<div class="leadership-grid__panel-container">
-									<?php the_post_thumbnail( 'leader' ); ?>
-									<div class="leadership-grid__name-group">
-										<h3 class="leadership-grid__name"><?php the_title(); ?></h3>
-										<div class="leadership-grid__position"><?php the_field( 'position' ); ?></div>
-										<div class="leadership-grid__company"><?php the_field( 'company' ); ?></div>
+			<div class="og-grid-container">
+				<ul class="og-grid leadership-grid__board">
+					<?php $directors = get_field( 'board_of_directors' );
+					if ( $directors ) {
+						foreach( $directors as $post ) {
+							setup_postdata( $post );
+							?>
+							<li id="post-<?php the_ID(); ?>-panel" <?php post_class( 'leadership-grid__panel panel' ); ?>>
+								<a href="<?php the_permalink(); ?>"
+									data-largesrc="<?php $largesrc = wp_get_attachment_image_src( get_post_thumbnail_id(), 'leader' ); echo esc_attr( $largesrc[0] ); ?>"
+									data-title="<?php echo esc_attr( get_the_title() ); ?>"
+									data-position="<?php echo esc_attr( get_field( 'position' ) ); ?>"
+									data-company="<?php echo esc_attr( get_field( 'company' ) ); ?>"
+									data-description="<?php echo esc_attr( get_field( 'bio' ) ); ?>">
+									<div class="leadership-grid__panel-container">
+										<?php the_post_thumbnail( 'leader' ); ?>
+										<div class="leadership-grid__name-group">
+											<h3 class="leadership-grid__name"><?php the_title(); ?></h3>
+											<div class="leadership-grid__position"><?php the_field( 'position' ); ?></div>
+											<div class="leadership-grid__company"><?php the_field( 'company' ); ?></div>
+										</div>
 									</div>
-								</div>
-							</a>
-						</li><!-- #post-## -->
-						<?php
-					}
-					wp_reset_postdata();
-				} ?>
-			</ul>
+								</a>
+							</li><!-- #post-## -->
+							<?php
+						}
+						wp_reset_postdata();
+					} ?>
+				</ul>
+			</div>
 		</div>
 	</div><!-- .content-below -->
 
--- wp-content/themes/ameris-bank/page-templates/lending-experts-page.php
+++ wp-content/themes/ameris-bank/page-templates/lending-experts-page.php
@@ -24,34 +24,36 @@ get_template_part( 'template-parts/page', 'banner' ); ?>
 				<?php } ?>
 
 				<div class="leadership-grid leadership-grid--experts">
-					<ul class="og-grid leadership-grid__management">
-						<?php $experts = get_field( 'lending_experts' );
-						if ( $experts ) {
-							foreach( $experts as $post ) {
-								setup_postdata( $post );
-								?>
-								<li id="post-<?php the_ID(); ?>-panel" class="leadership-grid__panel panel" <?php post_class(); ?>>
-									<a href="<?php the_permalink(); ?>"
-										data-largesrc="<?php $largesrc = wp_get_attachment_image_src( get_post_thumbnail_id(), 'leader' ); echo esc_attr( $largesrc[0] ); ?>"
-										data-title="<?php echo esc_attr( get_the_title() ); ?>"
-										data-position="<?php echo esc_attr( get_field( 'position/title' ) ); ?>"
-										data-phone="<?php echo esc_attr( get_field( 'phone' ) ); ?>"
-										data-email="<?php echo esc_attr( get_field( 'email' ) ); ?>"
-										data-description="<?php echo esc_attr( get_field( 'bio' ) ); ?>">
-										<div class="leadership-grid__panel-container">
-											<?php the_post_thumbnail( 'leader' ); ?>
-											<div class="leadership-grid__name-group">
-												<h4 class="leadership-grid__name"><?php the_title(); ?></h4>
-												<div class="leadership-grid__position"><?php the_field( 'position/title' ); ?></div>
+					<div class="og-grid-container">
+						<ul class="og-grid leadership-grid__management">
+							<?php $experts = get_field( 'lending_experts' );
+							if ( $experts ) {
+								foreach( $experts as $post ) {
+									setup_postdata( $post );
+									?>
+									<li id="post-<?php the_ID(); ?>-panel" class="leadership-grid__panel panel" <?php post_class(); ?>>
+										<a href="<?php the_permalink(); ?>"
+											data-largesrc="<?php $largesrc = wp_get_attachment_image_src( get_post_thumbnail_id(), 'leader' ); echo esc_attr( $largesrc[0] ); ?>"
+											data-title="<?php echo esc_attr( get_the_title() ); ?>"
+											data-position="<?php echo esc_attr( get_field( 'position/title' ) ); ?>"
+											data-phone="<?php echo esc_attr( get_field( 'phone' ) ); ?>"
+											data-email="<?php echo esc_attr( get_field( 'email' ) ); ?>"
+											data-description="<?php echo esc_attr( get_field( 'bio' ) ); ?>">
+											<div class="leadership-grid__panel-container">
+												<?php the_post_thumbnail( 'leader' ); ?>
+												<div class="leadership-grid__name-group">
+													<h4 class="leadership-grid__name"><?php the_title(); ?></h4>
+													<div class="leadership-grid__position"><?php the_field( 'position/title' ); ?></div>
+												</div>
 											</div>
-										</div>
-									</a>
-								</li><!-- #post-## -->
-								<?php
-							}
-							wp_reset_postdata();
-						} ?>
-					</ul>
+										</a>
+									</li><!-- #post-## -->
+									<?php
+								}
+								wp_reset_postdata();
+							} ?>
+						</ul>
+					</div>
 				</div>
 
 			<?php endwhile; // End of the loop. ?>
