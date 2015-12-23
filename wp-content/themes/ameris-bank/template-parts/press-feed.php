67d81fb065c2292a24c2e3b418bfe1d6ee5dc5c8
Michael Dance <michael.dance@agencychief.com>
2015-12-22 19:11:17 -0500

    removed temporary error check

--- wp-content/themes/ameris-bank/template-parts/press-feed.php
+++ wp-content/themes/ameris-bank/template-parts/press-feed.php
@@ -2,16 +2,12 @@
 	<?php 
 	$url = 'http://www.snl.com/IRWebLinkX/rss/prfeed.aspx?iid=100594';
 	$rss = fetch_feed( $url );
-	if ( is_wp_error( $rss ) ) {
-		echo $rss->get_error_message();
-	} else {
-		foreach ( $rss->get_items( 0, 3 ) as $item ) { ?>
-			<div class="recent-post">
-				<h3 class="recent-post__title"><a href="<?php echo $item->get_link(); ?>"><?php echo $item->get_title(); ?></a></h3>
-				<div class="recent-post__meta meta"><?php echo date( 'n/j/Y', strtotime( $item->get_date() ) ); ?></div>
-			</div>
-		<?php }
-	} ?>
+	foreach ( $rss->get_items( 0, 3 ) as $item ) { ?>
+		<div class="recent-post">
+			<h3 class="recent-post__title"><a href="<?php echo $item->get_link(); ?>"><?php echo $item->get_title(); ?></a></h3>
+			<div class="recent-post__meta meta"><?php echo date( 'n/j/Y', strtotime( $item->get_date() ) ); ?></div>
+		</div>
+	<?php } ?>
 	<div class="more-recent-posts">
 		<a class="more-recent-posts__link" href="<?php echo $url; ?>">More Press</a>
 	</div>
