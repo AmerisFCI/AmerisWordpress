c5cbdc2696c2a53f0d6e572461d3b7cdfe93b671
Michael Dance <michael.dance@agencychief.com>
2016-05-31 16:48:44 -0400

    added Google Tag Manager code to header

--- wp-content/themes/ameris-bank/header.php
+++ wp-content/themes/ameris-bank/header.php
@@ -36,11 +36,22 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WN3V8Z"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WN3V8Z');</script>
+<!-- End Google Tag Manager -->
+
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ameris-bank' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		
+
 		<div class="site-header__inner-wrap inner-wrap">
 
 			<div class="site-branding">
