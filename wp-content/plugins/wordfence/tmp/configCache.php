f637b0ac5253f7f810867dc54fa6f284152a42cf
Michael Dance <michael.dance@agencychief.com>
2016-06-10 14:25:39 -0400

    fixed gitignore thing

--- wp-content/plugins/wordfence/tmp/configCache.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* Wordfence temporary file security header */
-echo "Nothing to see here!\n"; exit(0);
-?>a:13:{s:6:"apiKey";s:160:"d9993e50c0f48a5099d8f6726a5b2aa7580799f19fc886164362797977f8128603087069f3b11e6f1944ebff77eeafe164bd2307149086fcd30a57ee214f950a2d7b6f21a7a70b23e6a579974d1cc880";s:9:"howGetIPs";s:0:"";s:11:"whitelisted";s:0:"";s:9:"cacheType";s:0:"";s:20:"loginSec_lockoutMins";s:1:"5";s:19:"other_hideWPVersion";s:1:"1";s:33:"scansEnabled_suspiciousAdminUsers";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:6:"isPaid";s:0:"";s:15:"firewallEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:26:"loginSec_disableAuthorScan";s:1:"1";s:25:"liveTraf_ignorePublishers";s:1:"1";}
